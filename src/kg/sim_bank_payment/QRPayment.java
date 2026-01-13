b37b97a78814fc774ceceff5aed0e7f0eb987d8c
orozobekovtair <tairorozobekov@gmail.com>
2026-01-13 10:11:51 +0600

    added sout hello, new class visapayment

--- src/kg/sim_bank_payment/QRPayment.java
+++ src/kg/sim_bank_payment/QRPayment.java
@@ -3,6 +3,7 @@ package kg.sim_bank_payment;
 public class QRPayment {
     public static void main(String[] args) {
         System.out.println("QR payment");
+        System.out.println("Hello");
 
     }
 }
