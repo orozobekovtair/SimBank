75d3871d7f0fd8321f6b0351a149dfe9c967abb4
orozobekovtair <tairorozobekov@gmail.com>
2026-01-14 21:31:15 +0600

    creat sout Tair

--- src/kg/sim_bank_payment/Regisration/Registration.java
+++ src/kg/sim_bank_payment/Regisration/Registration.java
@@ -3,5 +3,6 @@ package kg.sim_bank_payment.Regisration;
 public class Registration {
     public static void main(String[] args) {
         System.out.println("Registration");
+        System.out.println("Tair");
     }
 }
